270046614e90d0aac1be3f6d4104c84f9641b724
Toru Hisai <thisai@student.42tokyo.jp>
2020-11-08 14:49:27 +0900

    norm

--- get_next_line.h
+++ get_next_line.h
@@ -6,20 +6,19 @@
 /*   By: thisai <thisai@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/04 14:42:13 by thisai            #+#    #+#             */
-/*   Updated: 2020/11/07 16:06:22 by thisai           ###   ########.fr       */
+/*   Updated: 2020/11/08 14:48:47 by thisai           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#include <stddef.h>
+# include <stddef.h>
 
-int	get_next_line(int fd, char **line);
+int							get_next_line(int fd, char **line);
 
-typedef struct s_buffer_list t_buffer_list;
-
-struct s_buffer_list
+typedef struct s_buflist	t_buffer_list;
+struct						s_buflist
 {
 	int				fd;
 	char			buffer[BUFFER_SIZE];
@@ -29,16 +28,14 @@ struct s_buffer_list
 	int				eof;
 };
 
-typedef struct s_string_list t_string_list;
-
-struct s_string_list
+typedef struct s_strlist	t_string_list;
+struct						s_string_list
 {
-	char		*str;
+	char			*str;
 	size_t			size;
 	t_string_list	*next;
 };
 
-void	*ft_memcpy(void *dst, const void *src, size_t n);
-
+void						*ft_memcpy(void *dst, const void *src, size_t n);
 
 #endif
