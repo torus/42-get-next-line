78604bdc8ff622b2a633f733accba1b05d13b004
Toru Hisai <thisai@student.42tokyo.jp>
2020-12-19 15:54:41 +0900

    Norm for bonus

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -6,7 +6,7 @@
 /*   By: thisai <thisai@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/04 14:42:13 by thisai            #+#    #+#             */
-/*   Updated: 2020/11/08 16:05:34 by thisai           ###   ########.fr       */
+/*   Updated: 2020/12/19 15:54:08 by thisai           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,34 +15,31 @@
 
 # include <stddef.h>
 
-int							get_next_line(int fd, char **line);
+int				get_next_line(int fd, char **line);
 
-typedef struct s_buflist	t_buffer_list;
-struct						s_buflist
+typedef struct	s_buflist
 {
-	int				fd;
-	char			buffer[BUFFER_SIZE];
-	size_t			cursor;
-	size_t			size;
-	t_buffer_list	*next;
-	t_buffer_list	*prev;
-	int				occupied;
-};
+	int					fd;
+	char				buffer[BUFFER_SIZE];
+	size_t				cursor;
+	size_t				size;
+	struct s_buflist	*next;
+	struct s_buflist	*prev;
+	int					occupied;
+}				t_buffer_list;
 
-typedef struct s_strlist	t_string_list;
-struct						s_strlist
+typedef struct	s_strlist
 {
-	char			*str;
-	size_t			size;
-	t_string_list	*next;
-};
+	char				*str;
+	size_t				size;
+	struct s_strlist	*next;
+}				t_string_list;
 
-void						*ft_memcpy(void *dst, const void *src, size_t n);
-
-t_buffer_list				*find_buffer(t_buffer_list *buffers, int fd);
-t_buffer_list				*new_buffer_list(t_buffer_list *tail, int fd);
-int							load_to_buffer(t_buffer_list *buf, int fd);
-t_string_list				*new_string(t_string_list *tail,
-										const char *str, size_t size);
+t_buffer_list	*find_buffer(t_buffer_list *buffers, int fd);
+t_buffer_list	*new_buffer_list(t_buffer_list *tail, int fd);
+int				load_to_buffer(t_buffer_list *buf, int fd);
+t_string_list	*new_string(t_string_list *tail, const char *str, size_t size);
+void			clean_up_buffer(t_buffer_list *head, t_buffer_list *buf,
+								int status);
 
 #endif
